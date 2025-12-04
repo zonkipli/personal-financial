ac222f583f544219ac453b8d0d0327b19b3749d8
muharis <muharisharis13@gmail.com>
2025-12-04 21:36:26 +0700

    update

---
+++ server-dist/server.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = require("http");
+const next_1 = __importDefault(require("next"));
+const port = parseInt(process.env.PORT || "3000", 10);
+const app = (0, next_1.default)({ dev: false });
+const handle = app.getRequestHandler();
+app.prepare().then(() => {
+    (0, http_1.createServer)((req, res) => {
+        handle(req, res);
+    }).listen(port, () => {
+        console.log("Server running on port " + port);
+    });
+});
---
+++ server.ts
@@ -0,0 +1,14 @@
+import { createServer } from "http";
+import next from "next";
+
+const port = parseInt(process.env.PORT || "3000", 10);
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, () => {
+    console.log("Server running on port " + port);
+  });
+});
